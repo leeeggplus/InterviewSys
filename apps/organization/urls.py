e4d31e0cdec1285fce1214cab2116d905ee12612
Dan Li <ldan@microsoft.com>
2018-05-18 02:12:41 +0800

    Complete org

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from apps.organization.views import new_or_modify, all_orgs, delete
+from apps.organization.views import new, all_orgs, delete, modify
 
 
 urlpatterns= [    
     path('all_orgs/', all_orgs, name='organization_all_orgs'), 
-    path('new_or_modify/', new_or_modify, name='organization_new_or_modify'), 
+    path('new/', new, name='organization_new'), 
     path('delete/<int:org_id>/', delete, name='organization_delete'), 
+    path('modify/<int:org_id>/', modify, name='organization_modify'), 
 ];
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from .forms import OrganizationForm
 from .models import Organization
 
@@ -15,45 +15,27 @@ def all_orgs(request):
     return render(request, 'organization/orgs.html', context)
 
 
-def new_or_modify(request):
+
+def new(request):
+    '''
+    Create 
+    '''
     # POST
     if request.method == 'POST':
         form = OrganizationForm(request.POST)
 
         if form.is_valid():
-            org_name = form.cleaned_data['name']             
-
-            try:                                     
-                exist = Organization.objects.get(name=org_name)    
-                # exist - update org info  
-                exist.name = form.cleaned_data['name']
-                exist.manager_name = form.cleaned_data['manager_name']
-                exist.manager_alias = form.cleaned_data['manager_alias']
-                exist.manager_email = form.cleaned_data['manager_email']
-                exist.parent_org = form.cleaned_data['parent_org']
-                exist.save()  
-
-                # clear tags
-                exist.skills.clear()
-
-                for tag in form.cleaned_data['skills']:  
-                    exist.skills.add(tag)                
-
-            except Organization.DoesNotExist:
-                # does not exist, directly save the form                
-                form.save()
-            
+            form.save()  
             return redirect(all_orgs)   
 
     # GET
     form = OrganizationForm()
-
     context = {
         'type': 0,
         'form': form,
     }
-
     return render(request, 'organization/org_detail.html', context)
+
 
 
 def delete(request, org_id):
@@ -73,3 +55,46 @@ def delete(request, org_id):
     }
 
     return render(request, 'organization/orgs.html', context)
+
+
+def modify(request, org_id):
+    '''
+    modify organization details
+    '''
+    # GET
+    if request.method == 'GET':
+        org_to_modify = get_object_or_404(Organization, id=org_id)
+        form = OrganizationForm(instance=org_to_modify)
+
+        context = {
+            'type': 1,
+            'org_id': org_id,
+            'form': form,
+        }
+        return render(request, 'organization/org_detail.html', context)
+
+    # POST
+    else:
+        form = OrganizationForm(request.POST)        
+        if form.is_valid():        
+            # get existing instance
+            org_name = form.cleaned_data['name']
+            exist = Organization.objects.get(name=org_name)
+            exist.manager_name = form.cleaned_data['manager_name']
+            exist.manager_alias = form.cleaned_data['manager_alias']
+            exist.manager_email = form.cleaned_data['manager_email']
+            exist.parent_org = form.cleaned_data['parent_org']
+            exist.save() 
+
+            # clear & update tags.
+            exist.skills.clear()
+            for tag in form.cleaned_data['skills']:  
+                exist.skills.add(tag)    
+        
+
+        orgs = Organization.objects.all()
+        context = {
+            'orgs': orgs
+        }
+
+        return render(request, 'organization/orgs.html', context)   
