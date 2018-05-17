b7584eecbefd9e69a2a2e508305db1cf87a2fa0e
Dan Li <ldan@microsoft.com>
2018-05-18 01:28:47 +0800

    delete & display org

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.organization.views import new_or_modify_org, all_orgs
+from apps.organization.views import new_or_modify, all_orgs, delete
 
 
 urlpatterns= [    
-    path('all_orgs/', all_orgs, name='all'), 
-    path('new_or_modify/', new_or_modify_org, name='new_or_modify_org'), 
+    path('all_orgs/', all_orgs, name='organization_all_orgs'), 
+    path('new_or_modify/', new_or_modify, name='organization_new_or_modify'), 
+    path('delete/<int:org_id>/', delete, name='organization_delete'), 
 ];
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -5,6 +5,9 @@ from .models import Organization
 # Create your views here.
 
 def all_orgs(request):
+    '''
+    list all organizations in org dashboard
+    '''
     orgs = Organization.objects.all()
     context = {
         'orgs': orgs
@@ -12,7 +15,7 @@ def all_orgs(request):
     return render(request, 'organization/orgs.html', context)
 
 
-def new_or_modify_org(request):
+def new_or_modify(request):
     # POST
     if request.method == 'POST':
         form = OrganizationForm(request.POST)
@@ -51,3 +54,22 @@ def new_or_modify_org(request):
     }
 
     return render(request, 'organization/org_detail.html', context)
+
+
+def delete(request, org_id):
+    '''
+    delete organization by ID: org_id from URL
+    '''
+    try:
+        org_to_delete = Organization.objects.get(id=org_id)
+        org_to_delete.delete()
+
+    except Organization.DoesNotExist:
+        pass    
+
+    orgs = Organization.objects.all()
+    context = {
+        'orgs': orgs
+    }
+
+    return render(request, 'organization/orgs.html', context)
